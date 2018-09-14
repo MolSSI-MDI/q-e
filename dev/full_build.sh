6e251227ef594fdffa217770a24342712246dfd4
Taylor Barnes <tbarnes1@vt.edu>
2018-09-14 12:16:54 -0400

    Change build scripts to build MDI

--- dev/full_build.sh
+++ dev/full_build.sh
@@ -4,5 +4,10 @@ module load intel-mpi/2017u4
 
 cd ../
 
+# copy the MDI modules out of the subtree and into the appropriate directories
+cp mdi/molssi_driver_interface/mdi.c clib
+cp mdi/molssi_driver_interface/mdi.h clib
+cp mdi/molssi_driver_interface/mdi_f90.f90 Modules
+
 ./configure CC=icc CXX=icpc F77=ifort F90=ifort FC=ifort CPP=cpp MPIF90=mpiifort -enable-parallel -enable-openmp
 make -j 32 pw
--- dev/rebuild.sh
+++ dev/rebuild.sh
@@ -4,4 +4,9 @@ module load intel-mpi/2017u4
 
 cd ../
 
+# copy the MDI modules out of the subtree and into the appropriate directories
+cp mdi/molssi_driver_interface/mdi.c clib
+cp mdi/molssi_driver_interface/mdi.h clib
+cp mdi/molssi_driver_interface/mdi_f90.f90 Modules
+
 make -j 32 pw
